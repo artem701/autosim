b0ebf7b1a0d9895eb68cce084ab893f4fd6ba649
Artem Dubrovskikh <artdubrovskikh@gmail.com>
2023-10-26 11:17:52 +0300

    update rendering sample

--- sample/render-acar/__main__.py
+++ sample/render-acar/__main__.py
@@ -1,25 +1,75 @@
 
+from time import time
 from autosim.car import ACar
-from eventloop import EventLoop
-from eventloop.events import Terminate
+from eventloop import Event, EventLoop, Listener
+from eventloop.events import Terminate, Iteration
 from renderer import Renderer
+from renderer.events import FrameRendered
 from simulation import Environment
+from simulation.environment.events import UpdateRequest, Tick
 from simulation.location import Circle, CircleSpace
 from timer.timer import Timer
 
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+logging.info('initializing...')
+
+class Watcher(Listener):
+    def __init__(self, timeout=25, logging_interval=1):
+        self.timeout = timeout
+        self.last_time = 0
+        self.iterations = 0
+        self.logging_interval = logging_interval
+
+    def input_events(self) -> set:
+        return Iteration, Tick, FrameRendered
+    
+    def accept(self, event: Event) -> list[Event]:
+        if isinstance(event, Iteration):
+            self.iterations += 1
+            return UpdateRequest()
+
+        if isinstance(event, Tick):
+            if self.logging_interval > 0 and event.environment.time - self.last_time >= self.logging_interval:
+                logging.info(f"time: {event.environment.time:.2f} seconds")
+                self.last_time += self.logging_interval
+
+            if event.environment.time >= self.timeout:
+                return Terminate(False)
+
+        if isinstance(event, FrameRendered):
+            pass
 
 loop = EventLoop()
 
+L = 500
+T = 20
+FPS = 24
+W = 1024
+H = 1024
+CARS = 10
 environment = Environment()
-car = ACar('50 * dt', ACar.Mode.MOVEMENT, Circle(CircleSpace(1000)))
-renderer = Renderer()
-timer = Timer(60, event=Terminate, ctx=False)
+cars = [ ACar(f"{50 / (i+1)} * dt", ACar.Mode.MOVEMENT, Circle(CircleSpace(L), -L // (i + 2))) for i in range(CARS) ]
+renderer = Renderer(fps=FPS, width=W, height=H)
+watcher = Watcher(T)
 
 loop.subscribe(environment)
-loop.subscribe(car)
+for car in cars:
+    loop.subscribe(car)
 loop.subscribe(renderer)
-loop.subscribe(timer)
+loop.subscribe(watcher)
 
+logging.info('simulating...')
+start = time()
 loop.loop()
+end = time()
+sim_time = end - start
 
+logging.info('rendering...')
+start = time()
 renderer.render('output')
+end = time()
+render_time = end - start
+
+logging.info(f"Simulated {T}s ({FPS*T} {W}x{H} frames, {watcher.iterations} iterations) in {sim_time:.2f} real seconds, render for {render_time:.2f} seconds")
