52510ab785c4b2fa68f828ca622935d904801023
Artem Dubrovskikh <artdubrovskikh@gmail.com>
2023-11-06 18:45:58 +0300

    fix test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 source env.sh
 
 LOG_FILE='pytest.log'
-python3 -m pytest $* 4<&1 5<&2 1>&2>&>(tee >(sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g' > $LOG_FILE))
+python3 -m pytest tests $* 4<&1 5<&2 1>&2>&>(tee >(sed -r 's/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g' > $LOG_FILE))
